999c867746ece724795f253a0fedd712f60ee631
John Domingo <johnwill.domingo@gmail.com>
2015-12-07 02:29:28 -0800

    Complete stringifyJSON tests for arrays and objects with proper funcition and undefined exclusions

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,13 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var str = '';
+
   if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
-    return obj;
+    return '' + obj;
   } else if (typeof obj === 'string') {
     return '"' + obj + '"';
   } else if (Array.isArray(obj)) {
-    
-  } else if (obj instaceof Object) {
+    if (obj[0] === undefined) {
+      return '[]';
+    } else {
+      str = '[';
 
+      for (var i = 0; i < obj.length; i++) {
+        str += stringifyJSON(obj[i]) + (i < obj.length - 1 ? ',' : '');
+      }
+
+      return str + ']'
+    }
+  } else if (obj instanceof Object) {
+    if (obj === undefined) {
+      return '{}';
+    } else {
+      str = '{';
+
+      for (var item in obj) {
+        if (typeof obj[item] !== 'function' && obj[item] !== undefined) {
+          str += stringifyJSON(item) + ':' + stringifyJSON(obj[item]) + ',';
+        }
+      }
+
+      return (str.charAt(str.length - 1) === ',' ? str.slice(0, -1) : str) + '}'
+    }
   }
 };
